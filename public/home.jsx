dfa02f57e981466827134a408ce39ed73a468c92
Mikhail Harchevnikov <mihmbox@gmail.com>
2015-09-27 18:46:10 +0300

    .

--- public/home.jsx
+++ public/home.jsx
@@ -1,7 +1,7 @@
 'use strict';
 
 import React from 'react';
-import SignInForm from './components/SignUpForm.jsx';
+import SignInForm from './components/SignInForm.jsx';
 
 React.render(<SignInForm showHeader={false} />, document.getElementById('signin-form'));
 
--- public/signup.jsx
+++ public/signup.jsx
@@ -1,13 +1,13 @@
-'use strict';
-
-import React from 'react';
-import SignUpForm from './components/SignUpForm.jsx';
-
-React.render(
-    (
-        <div className="row valign-wrapper">
-            <div className="valign col l3 offset-l4 m6 offset-m3 s12">
-                <SignUpForm />
-            </div>
-        </div>
-    ), document.getElementById('signup-app'));
+//'use strict';
+//
+//import React from 'react';
+//import SignUpForm from './components/SignInForm.jsx';
+//
+//React.render(
+//    (
+//        <div className="row valign-wrapper">
+//            <div className="valign col l3 offset-l4 m6 offset-m3 s12">
+//                <SignUpForm />
+//            </div>
+//        </div>
+//    ), document.getElementById('signup-app'));
